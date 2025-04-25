33799cf7026b2fead33a7c8db5141b4b535a0609
Majok-Bol <bolmajok001@gmail.com>
2025-04-25 12:47:10 +0300

    changed drawer background color

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-//handle home page
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,38 +6,50 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        appBar:AppBar(
-          title: Text('SimpleNoteX'),
-          centerTitle: true,
-          backgroundColor: Colors.grey.shade200,
-        leading: Builder(builder: (context)=>IconButton(onPressed:() {
-      Scaffold.of(context).openDrawer();
-
-    }, icon:Icon(Icons.menu,color: Colors.black,)),
-
-    )
-        ),
-      drawer: Drawer(
-        surfaceTintColor: Colors.blue.shade900,
-        width: 250,
-
+      appBar: AppBar(
+        title: Text('NoteFlow'),
+        centerTitle:true,
+        backgroundColor: Colors.grey.shade300,
+        leading: Builder(builder:(context)=>IconButton(onPressed:(){
+          Scaffold.of(context).openDrawer();
+        },icon: Icon(Icons.menu),)),
+      ),
+      drawer:Drawer(
+        backgroundColor: Colors.grey.shade100,
         child: Column(
           children: [
-            DrawerHeader(child:CircleAvatar(radius: 100,backgroundColor: Colors.grey.shade400,)),
-            Column(
-              children: [
-                Padding(padding:EdgeInsets.all(1),child: ListTile(onTap:(){},leading:Icon(Icons.sticky_note_2_outlined),title:Text('View notes'))),
+            DrawerHeader(child: CircleAvatar(radius: 60,backgroundColor: Colors.purple.shade200,)),
+            Padding(padding: EdgeInsets.all(2),child: ListTile(
+              onTap:() {
 
-                Padding(padding:EdgeInsets.all(1),child: ListTile(onTap:(){},leading:Icon(Icons.favorite_border),title:Text('Favorites'))),
-                Padding(padding:EdgeInsets.all(1),child: ListTile(onTap:(){},leading:Icon(Icons.sunny),title:Text('Switch mode'))),
-                Padding(padding:EdgeInsets.all(1),child: ListTile(onTap:(){},leading:Icon(Icons.logout),title:Text('Logout'))),
-              ],
-            )
+              },
+              title:Text('View notes'),
+              leading: Icon(Icons.sticky_note_2_outlined),),),
+            Padding(padding: EdgeInsets.all(2),
+              child: ListTile(
+                onTap:() {
+
+                },
+                title:Text('Favorites'),
+                leading: Icon(Icons.favorite),),),
+            Padding(padding: EdgeInsets.all(2),
+              child: ListTile(
+                onTap:() {
+
+                },
+                title:Text('Switch Mode'),
+                leading: Icon(Icons.sunny),),),
+            Padding(padding: EdgeInsets.all(2),
+              child: ListTile(
+                onTap:() {
+
+                },
+                title:Text('Logout'),
+      leading: Icon(Icons.logout),),),
           ],
         ),
-
       ),
     );
-
   }
+
 }
